ea5d9fae0d493b1ed10f3c97dac7c4e05b021cc2
Matheus Almeida <2xplodxg@gmail.com>
2016-08-19 14:55:51 -0300

    Primeiro UP
    Matheus

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var usuario = "Marcos Almeida"
+
+print (str + " by " + usuario)
+
+var i = 1
+
+while i < 10 {
+    
+    print(i)
+    
+    i = i * 2
+    
+}
